06e488917a1344cfda27bc31a8bc81e5ecb062ad
mehdi shirani <m3hdi.shirani@gmail.com>
2022-09-11 11:17:26 +0430

    App Collapse

---
+++ components/AppCollapse.vue
@@ -0,0 +1,61 @@
+<template>
+    <div :class="{
+        'bg-gray-100':isOpen && !noStyle,
+        'py-3 px-3.5 t-row justify-between cursor-pointer rounded transition-all lg:hover:bg-gray-200':
+        !noStyle
+    }" @click="toggle" v-bind="$attrs">
+        <slot name="title" :is-open="isOpen" />
+        <div class="mr-3" v-if="withTitleIcon">{{ isOpen ? "-" : "+" }}</div>
+    </div>
+    <div ref="target" class="px-1.5 h-0 overflow-y-hidden">
+        <slot />
+    </div>
+</template>
+
+<script setup lang="ts">
+import { gsap } from "gsap";
+import { useDebounceFn, useEventListener } from "@vueuse/core";
+
+const props = defineProps({
+    noStyle: {
+        type: Boolean,
+        default: false
+    },
+    withTitleIcon: {
+        type: Boolean,
+        default: true,
+    },
+})
+const target = ref<HTMLElement | null>(null)
+const progress = ref(0)
+const isOpen = computed(() => unref(progress) !== 0)
+
+let animation: any;
+
+const setAnimation = () => {
+    animation = gsap.to(unref(target), {
+        height: 'auto',
+        overflow: "hidden",
+        paused: true,
+        duration: 0.3,
+        onUpdate() {
+            progress.value = animation.progress();
+        },
+    })
+}
+onMounted(() => {
+    setAnimation();
+});
+
+const toggle = () => {
+    if (animation.progress() === 0) {
+        animation?.play();
+    } else {
+        animation?.reverse();
+    }
+};
+const debouncedFn = useDebounceFn(() => {
+    animation.progress(0).invalidate();
+}, 500);
+useEventListener("resize", debouncedFn);
+</script>
---
+++ pages/scrollnav.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="flex flex-col space-y-3 justify-center items-center">
+        <div class="sticky top-0 bg-white py-4 z-10">
+            <app-scroll-to-nav :items="CourseTabs"></app-scroll-to-nav>
+        </div>
+        <template v-for="item in CourseTabs" :key="`course-item-${item.id}`">
+            <div class="h-96 w-full flex flex-col items-center justify-center my-96 bg-green-200 p-8">
+                <h2 :id="item.id" class="font-bold text-4xl">{{ item.label }}</h2>
+            </div>
+        </template>
+    </div>
+</template>
+
+
+<script setup lang="ts">
+const CourseTabs = [
+    { id: "where-to-start", label: "از کجا شروع کنم؟" },
+    { id: "register-verification", label: "نحوه ثبت‌نام و احراز هویت" },
+    { id: "sell-buy", label: "خرید و فروش ارز دیجیتال" },
+    { id: "withdraw-deposit", label: "واریز و برداشت ارز دیجیتال در کیف پول" },
+    { id: "referral-marketing", label: "طرح دعوت از دوستان در گرندبیتکس" },
+];
+</script>
